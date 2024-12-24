97c67c1f4c5528e906db8f4b1ae42da8c1bb6e09
meta-d <meta.digital.cloud@gmail.com>
2024-12-25 01:07:58 +0800

    fix: production env

--- packages/server/src/feature/feature.subscriber.ts
+++ packages/server/src/feature/feature.subscriber.ts
@@ -1,37 +1,37 @@
-import { pacToggleFeatures } from "@metad/server-config";
-import { FeatureStatusEnum } from "@metad/contracts";
-import { EntitySubscriberInterface, EventSubscriber } from "typeorm";
-import { shuffle } from 'underscore';
-import { FileStorage } from "./../core/file-storage";
-import { Feature } from "./feature.entity";
+import { FeatureStatusEnum } from '@metad/contracts'
+import { toggleFeatures } from '@metad/server-config'
+import { isNil } from 'lodash'
+import { EntitySubscriberInterface, EventSubscriber } from 'typeorm'
+import { shuffle } from 'underscore'
+import { FileStorage } from './../core/file-storage'
+import { Feature } from './feature.entity'
 
 @EventSubscriber()
 export class FeatureSubscriber implements EntitySubscriberInterface<Feature> {
+	/**
+	 * Indicates that this subscriber only listen to Feature events.
+	 */
+	listenTo() {
+		return Feature
+	}
 
-    /**
-    * Indicates that this subscriber only listen to Feature events.
-    */
-    listenTo() {
-        return Feature;
-    }
-
-    /**
-    * Called after entity is loaded.
-    */
-    afterLoad(entity: any) {
-        if (!entity.status) {
-			entity.status = shuffle(Object.values(FeatureStatusEnum))[0];
+	/**
+	 * Called after entity is loaded.
+	 */
+	afterLoad(entity: any) {
+		if (!entity.status) {
+			entity.status = shuffle(Object.values(FeatureStatusEnum))[0]
 		}
 
-        if (pacToggleFeatures.hasOwnProperty(entity.code)) {
-			const feature = pacToggleFeatures[entity.code];
-			entity.isEnabled = feature;
+		if (!isNil(toggleFeatures[entity.code])) {
+			const feature = toggleFeatures[entity.code]
+			entity.isEnabled = feature
 		} else {
-			entity.isEnabled = true;
+			entity.isEnabled = true
 		}
 
-        if (entity.image) {
-			entity.imageUrl = new FileStorage().getProvider().url(entity.image);
+		if (entity.image) {
+			entity.imageUrl = new FileStorage().getProvider().url(entity.image)
 		}
-    }
+	}
 }
