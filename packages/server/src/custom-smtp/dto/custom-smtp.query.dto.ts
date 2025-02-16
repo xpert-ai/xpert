92bd9855e3df890daa54dd7ee14f62bfd1e194d9
meta-d <meta.digital.cloud@gmail.com>
2025-02-16 09:50:30 +0800

    fix: disable validation for custom smtp

--- packages/server/src/custom-smtp/dto/custom-smtp.query.dto.ts
+++ packages/server/src/custom-smtp/dto/custom-smtp.query.dto.ts
@@ -11,6 +11,6 @@ export class CustomSmtpQueryDTO implements ICustomSmtpFindInput {
 	@ApiPropertyOptional({ type: () => String })
 	@IsOptional()
 	@IsUUID()
-	@IsOrganizationBelongsToUser()
+	// @IsOrganizationBelongsToUser()
 	readonly organizationId: IOrganization['id'];
 }
