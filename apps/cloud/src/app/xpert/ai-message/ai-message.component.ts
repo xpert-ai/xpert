2109b290c23998249eec68de4ddbf0d14b86da7a
meta-d <meta.digital.cloud@gmail.com>
2025-03-22 19:26:38 +0800

    feat: expand submessage

--- apps/cloud/src/app/xpert/ai-message/ai-message.component.ts
+++ apps/cloud/src/app/xpert/ai-message/ai-message.component.ts
@@ -1,16 +1,27 @@
 import { DragDropModule } from '@angular/cdk/drag-drop'
 import { CdkMenuModule } from '@angular/cdk/menu'
 import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component, booleanAttribute, computed, effect, inject, input, signal } from '@angular/core'
+import {
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  inject,
+  input,
+  signal
+} from '@angular/core'
+import { toObservable } from '@angular/core/rxjs-interop'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { MatIconModule } from '@angular/material/icon'
 import { MatTooltipModule } from '@angular/material/tooltip'
 import { RouterModule } from '@angular/router'
 import { nonNullable, stringifyMessageContent } from '@metad/copilot'
+import { ListHeightStaggerAnimation } from '@metad/core'
 import { NgmCommonModule } from '@metad/ocap-angular/common'
 import { Indicator, omit } from '@metad/ocap-core'
 import { TranslateModule } from '@ngx-translate/core'
 import { MarkdownModule } from 'ngx-markdown'
+import { filter, map, shareReplay, switchMap, tap } from 'rxjs'
 import {
   ChatMessageFeedbackRatingEnum,
   ChatMessageFeedbackService,
@@ -19,20 +30,18 @@ import {
   IChatMessage,
   injectToastr,
   isMessageGroup,
+  TMessageContentComplex,
   XpertAgentExecutionService,
   XpertAgentExecutionStatusEnum
 } from '../../@core'
 import { EmojiAvatarComponent } from '../../@shared/avatar'
-import { ChatService } from '../chat.service'
-import { ChatComponentMessageComponent } from '../component-message/component-message.component'
-import { TCopilotChatMessage } from '../types'
-import { toObservable } from '@angular/core/rxjs-interop'
-import { filter, map, shareReplay, switchMap, tap } from 'rxjs'
 import { ChatMessageExecutionComponent } from '../../@shared/chat'
 import { CopyComponent } from '../../@shared/common'
-import { ListHeightStaggerAnimation } from '@metad/core'
+import { ChatService } from '../chat.service'
+import { ChatComponentMessageComponent } from '../component-message/component-message.component'
 import { XpertHomeService } from '../home.service'
 import { XpertOcapService } from '../ocap.service'
+import { TCopilotChatMessage } from '../types'
 
 @Component({
   standalone: true,
@@ -47,13 +56,12 @@ import { XpertOcapService } from '../ocap.service'
     CdkMenuModule,
     MarkdownModule,
     MatTooltipModule,
-    MatIconModule,
     NgmCommonModule,
     EmojiAvatarComponent,
     ChatComponentMessageComponent,
     ChatMessageExecutionComponent,
     CopyComponent,
-    DateRelativePipe,
+    DateRelativePipe
   ],
   selector: 'pac-ai-message',
   templateUrl: './ai-message.component.html',
@@ -82,9 +90,7 @@ export class ChatAiMessageComponent {
   readonly feedbacks = this.chatService.feedbacks
   readonly executionId = computed(() => this.message()?.executionId)
   readonly status = computed(() => this.message()?.status)
-  readonly answering = computed(() =>
-    this.chatService.answering() && ['thinking', 'answering'].includes(this.status())
-  )
+  readonly answering = computed(() => this.chatService.answering() && ['thinking', 'answering'].includes(this.status()))
 
   readonly #contentStr = computed(() => {
     const content = this.message()?.content
@@ -110,7 +116,8 @@ export class ChatAiMessageComponent {
   readonly contents = computed(() => {
     const contents = this.message()?.content
     if (Array.isArray(contents)) {
-      return this.canvasType() === 'Dashboard' ? contents.filter((data) => !(data.type === 'component' && data.data?.category === 'Dashboard'))
+      return this.canvasType() === 'Dashboard'
+        ? contents.filter((data) => !(data.type === 'component' && data.data?.category === 'Dashboard'))
         : contents
     }
     return null
@@ -123,7 +130,9 @@ export class ChatAiMessageComponent {
 
   readonly contentString = computed(() => stringifyMessageContent(this.message().content))
 
-  readonly executings = computed(() => this.message().executions?.filter((_) => _.status === XpertAgentExecutionStatusEnum.RUNNING))
+  readonly executings = computed(() =>
+    this.message().executions?.filter((_) => _.status === XpertAgentExecutionStatusEnum.RUNNING)
+  )
 
   readonly expandExecutions = signal(false)
   readonly loadingExecutions = signal(false)
@@ -135,7 +144,8 @@ export class ChatAiMessageComponent {
     tap(() => this.loadingExecutions.set(false)),
     map((execution) => {
       const executions = []
-      execution.subExecutions?.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+      execution.subExecutions
+        ?.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
         .forEach((_) => executions.push(_))
       executions.push(omit(execution, 'subExecutions'))
       return executions
@@ -149,7 +159,9 @@ export class ChatAiMessageComponent {
 
   // Steps
   readonly #steps = computed(() => this.message().steps)
-  readonly lastStep = computed(() => this.canvasMessageId() !== this.message().id && this.#steps() ? this.#steps()[this.#steps().length - 1] : null)
+  readonly lastStep = computed(() =>
+    this.canvasMessageId() !== this.message().id && this.#steps() ? this.#steps()[this.#steps().length - 1] : null
+  )
   readonly steps = computed(() => {
     if (this.expandSteps()) {
       return this.#steps()
@@ -158,19 +170,22 @@ export class ChatAiMessageComponent {
     }
   })
   readonly expandSteps = signal(false)
-  readonly canvasMessageId = computed(() =>
-    this.homeService.canvasOpened()?.type === 'Computer' &&
-      this.homeService.canvasOpened()?.messageId
+  readonly canvasMessageId = computed(
+    () => this.homeService.canvasOpened()?.type === 'Computer' && this.homeService.canvasOpened()?.messageId
   )
   readonly canvasType = computed(() => this.homeService.canvasOpened()?.type)
 
   // Agents
   readonly conversation = this.chatService.conversation
   readonly xpert = this.chatService.xpert
-  readonly agents = computed(() => this.xpert()?.agents?.reduce((acc, agent) => {
-    acc[agent.key] = agent;
-    return acc;
-  }, {}) ?? {})
+  readonly agents = computed(
+    () =>
+      this.xpert()?.agents?.reduce((acc, agent) => {
+        acc[agent.key] = agent
+        return acc
+      }, {}) ?? {}
+  )
+  readonly collapseMessages = signal<Record<string, boolean>>({})
 
   constructor() {
     effect(() => {
@@ -238,5 +253,11 @@ export class ChatAiMessageComponent {
       type: 'Computer',
       messageId: this.message().id
     })
+  }
+
+  toggleCollapseMessage(message: TMessageContentComplex) {
+    if (message.type === 'text') {
+      this.collapseMessages.update((state) => ({ ...state, [message.id]: !state[message.id] }))
+    }
   }
 }
