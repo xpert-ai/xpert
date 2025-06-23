e8d3d1cb64229b7a80c2663a95aab9ecefea5ee1
meta-d <meta.digital.cloud@gmail.com>
2025-06-23 11:51:59 +0800

    fix: pro tools

--- packages/server-ai/src/sandbox/tools/file/tools/list.ts
+++ packages/server-ai/src/sandbox/tools/file/tools/list.ts
@@ -1,13 +1,10 @@
 import { CallbackManagerForToolRun } from '@langchain/core/callbacks/manager'
 import { LangGraphRunnableConfig } from '@langchain/langgraph'
 import { Logger } from '@nestjs/common'
-import * as _axios from 'axios'
 import z from 'zod'
-import { SandboxLoadCommand } from '../../../../../sandbox/commands'
 import { SandboxBaseTool } from '../../sandbox-tool'
 import { FileToolset } from '../file'
 import { FileToolEnum } from '../types'
-const axios = _axios.default
 
 export class FileListTool extends SandboxBaseTool {
 	readonly #logger = new Logger(FileListTool.name)
@@ -27,17 +24,8 @@ export class FileListTool extends SandboxBaseTool {
 	async _call(parameters: object, callbacks: CallbackManagerForToolRun, config: LangGraphRunnableConfig & { toolCall }) {
 		const { signal, configurable } = config ?? {}
 
-		const {sandboxUrl} = await this.toolset.commandBus.execute<SandboxLoadCommand, {sandboxUrl: string}>(new SandboxLoadCommand())
-
-		const requestData = {
-			...parameters,
-			thread_id: configurable?.thread_id
-		}
-
 		try {
-			const result = await axios.post(`${sandboxUrl}/file/list/`, requestData, { signal })
-
-			return JSON.stringify(result.data)
+			//
 		} catch (error) {
 			throw error.response?.data?.detail || error.response?.data || error
 		}
