3afae14fcfa3376f9eedd1f93459729a08a4984a
meta-d <meta.digital.cloud@gmail.com>
2025-06-25 13:05:09 +0800

    fix(agent): set context variable current state for toolNode

--- packages/server-ai/src/shared/agent/tool_node.ts
+++ packages/server-ai/src/shared/agent/tool_node.ts
@@ -9,9 +9,10 @@ import { RunnableConfig, RunnableToolLike } from "@langchain/core/runnables";
 import { DynamicTool, StructuredToolInterface } from "@langchain/core/tools";
 import { Command, isCommand, isGraphInterrupt, MessagesAnnotation } from "@langchain/langgraph";
 import { END, Send } from "@langchain/langgraph";
-import { ChatMessageEventTypeEnum } from "@metad/contracts";
+import { ChatMessageEventTypeEnum, CONTEXT_VARIABLE_CURRENTSTATE } from "@metad/contracts";
 import { getErrorMessage } from "@metad/server-common";
 import { RunnableCallable } from "./utils";
+import { setContextVariable } from "@langchain/core/context";
 
 export type ToolNodeOptions = {
   name?: string;
@@ -165,6 +166,9 @@ export class ToolNode<T = any> extends RunnableCallable<T, T> {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async run(input: any, config: RunnableConfig): Promise<T> {
+    // We set a context variable before invoking the tool node and running our tool.
+    setContextVariable(CONTEXT_VARIABLE_CURRENTSTATE, input)
+    
     const message = Array.isArray(input)
       ? input[input.length - 1]
       : input.messages[input.messages.length - 1];
