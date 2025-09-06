8373b0238dbb90f43c062ce8af4002b67a29d051
Xiaosu <suhe@njust.edu.cn>
2025-09-06 13:45:41 +0800

    feat: remove duplicate handler of command

--- packages/server/src/organization/commands/handlers/organization.demo.handler.ts
+++
@@ -1,9 +0,0 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
-import { OrganizationDemoCommand } from '../organization.demo.command'
-
-@CommandHandler(OrganizationDemoCommand)
-export class OrganizationDemoHandler implements ICommandHandler<OrganizationDemoCommand> {
-  public async execute(command: OrganizationDemoCommand): Promise<void> {
-    return Promise.resolve()
-  }
-}
