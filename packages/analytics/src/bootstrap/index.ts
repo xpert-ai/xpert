9cfa604520798df8a941723362fc4ac1ff5b7437
Tiven Wang <i.tiven.wang@gmail.com>
2025-10-26 22:07:41 +0800

    migrate out plugin unstructured

--- packages/analytics/src/bootstrap/index.ts
+++ packages/analytics/src/bootstrap/index.ts
@@ -144,7 +144,6 @@ export async function preBootstrapPlugins() {
 			'@xpert-ai/plugin-retriever-common',
 			'@xpert-ai/plugin-tool-calculator',
 			'@xpert-ai/plugin-transformer-common',
-			'@xpert-ai/plugin-transformer-unstructured',
 			'@xpert-ai/plugin-vlm-default',
 			'@xpert-ai/plugin-vstore-chroma',
 			'@xpert-ai/plugin-vstore-milvus',
