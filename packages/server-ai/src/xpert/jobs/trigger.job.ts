a88f6bc56c39c0b231527de07611a5344ef5dc0f
Tiven Wang <i.tiven.wang@gmail.com>
2025-11-11 11:35:06 +0800

    feat: trigger of schedule strategy

--- packages/server-ai/src/xpert/jobs/trigger.job.ts
+++ packages/server-ai/src/xpert/jobs/trigger.job.ts
@@ -1,5 +1,6 @@
+import { STATE_VARIABLE_HUMAN } from '@metad/contracts'
 import { isNil } from '@metad/server-common'
-import { runWithRequestContext, UserService } from '@metad/server-core'
+import { runWithRequestContext as _runWithRequestContext, UserService } from '@metad/server-core'
 import { JOB_REF, Process, Processor } from '@nestjs/bull'
 import { Inject, Logger } from '@nestjs/common'
 import { CommandBus } from '@nestjs/cqrs'
@@ -10,7 +11,7 @@ import { map } from 'rxjs/operators'
 import { XpertChatCommand } from '../commands'
 import { QUEUE_XPERT_TRIGGER, TTriggerJob } from '../types'
 import { XpertService } from '../xpert.service'
-import { STATE_VARIABLE_HUMAN } from '@metad/contracts'
+import { runWithRequestContext } from '@xpert-ai/plugin-sdk'
 
 @Processor({
 	name: QUEUE_XPERT_TRIGGER
@@ -34,39 +35,45 @@ export class XpertTriggerConsumer {
 		const xpert = await this.xpertService.findOne(job.data.xpertId)
 		const userId = job.data.userId || xpert.createdById
 		const user = await this.userService.findOne(userId, { relations: ['role'] })
-		runWithRequestContext({ user, headers: { ['organization-id']: xpert.organizationId, language: user.preferredLanguage } }, async () => {
-			// Chat with xpert
-			const stream = await this.commandBus.execute<XpertChatCommand, Observable<MessageEvent>>(
-				new XpertChatCommand(
-					{
-						input: job.data[STATE_VARIABLE_HUMAN],
-						xpertId: job.data.xpertId,
-						state: job.data.state,
-					},
-					{
-						from: job.data.from,
-						isDraft: job.data.isDraft,
-						execution: job.data.executionId ? { id: job.data.executionId } : null
-					}
-				)
-			)
-			stream
-				.pipe(
-					map((message: any) => {
-						if (typeof message.data.data === 'object') {
-							return {
-								...message,
-								data: {
-									...message.data,
-									data: omitBy(message.data.data, isNil) // Remove null or undefined values
-								}
+		runWithRequestContext({ user, headers: { ['organization-id']: xpert.organizationId, language: user.preferredLanguage } }, null, async () => {
+			_runWithRequestContext({ user, headers: { ['organization-id']: xpert.organizationId, language: user.preferredLanguage } }, async () => {
+				try {
+					// Chat with xpert
+					const stream = await this.commandBus.execute<XpertChatCommand, Observable<MessageEvent>>(
+						new XpertChatCommand(
+							{
+								input: job.data[STATE_VARIABLE_HUMAN],
+								xpertId: job.data.xpertId,
+								state: job.data.state,
+							},
+							{
+								from: job.data.from,
+								isDraft: job.data.isDraft,
+								execution: job.data.executionId ? { id: job.data.executionId } : null
 							}
-						}
+						)
+					)
+					stream
+						.pipe(
+							map((message: any) => {
+								if (typeof message.data.data === 'object') {
+									return {
+										...message,
+										data: {
+											...message.data,
+											data: omitBy(message.data.data, isNil) // Remove null or undefined values
+										}
+									}
+								}
 
-						return message
-					})
-				)
-				.subscribe()
+								return message
+							})
+						)
+						.subscribe()
+				} catch(err) {
+					console.error(err)
+				}
+			})
 		})
 	}
 }
