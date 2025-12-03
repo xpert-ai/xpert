901b25acee07eb53266086b09ccb72920c751399
Tiven Wang <i.tiven.wang@gmail.com>
2025-12-03 19:03:31 +0800

    fix: cache ttl & web terminal focus

--- packages/server-ai/src/chat-conversation/queries/conversation-get.query.ts
+++ packages/server-ai/src/chat-conversation/queries/conversation-get.query.ts
@@ -1,15 +1,18 @@
 import { IChatConversation } from '@metad/contracts'
 import { FindOptionsWhere } from '@metad/server-core'
-import { IQuery } from '@nestjs/cqrs'
+import { Query } from '@nestjs/cqrs'
+import { ChatConversation } from '../conversation.entity'
 
 /**
  * Find one chat conversation by conditions, not raise exception when not found
  */
-export class GetChatConversationQuery implements IQuery {
+export class GetChatConversationQuery extends Query<ChatConversation> {
 	static readonly type = '[Chat Conversation] Find One'
 
 	constructor(
 		public readonly conditions: FindOptionsWhere<IChatConversation>,
 		public readonly relations?: string[]
-	) {}
+	) {
+		super()
+	}
 }
