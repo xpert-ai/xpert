79ad7d3cc69eded19fb97eb5453470779e9e0202
ponyfly6 <2083323301@qq.com>
2025-12-24 13:52:45 +0800

    fix: update uniqBy call to use key extractor function

--- packages/echarts/src/lib/smart-echarts.ts
+++ packages/echarts/src/lib/smart-echarts.ts
@@ -399,7 +399,7 @@ export class SmartEChartEngine extends SmartChartEngine<SmartChartEngineState> {
       const category = getChartCategory(chartAnnotation)
       const chartSeries = getChartSeries(chartAnnotation)
 
-      const dimensions = uniqBy(chartAnnotation.dimensions, (a, b) => (a.hierarchy || a.dimension) === (b.hierarchy || b.dimension))
+      const dimensions = uniqBy(chartAnnotation.dimensions, (item) => item.hierarchy || item.dimension)
 
       const children = event.selected?.length ?
         event.selected
