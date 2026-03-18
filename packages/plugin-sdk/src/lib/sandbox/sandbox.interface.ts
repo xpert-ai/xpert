c8aaa567bf15979546395f2a785fbaadd38a4590
Tiven.Wang <i.tiven.wang@gmail.com>
2026-03-18 17:36:18 +0800

    feat: acquire backend

--- packages/plugin-sdk/src/lib/sandbox/sandbox.interface.ts
+++ packages/plugin-sdk/src/lib/sandbox/sandbox.interface.ts
@@ -1,38 +1,42 @@
-import { TSandboxProviderMeta } from "@metad/contracts"
-import { SandboxBackendProtocol } from "./protocol"
+import { TSandboxProviderMeta, TSandboxWorkForType } from '@metad/contracts'
+import { SandboxBackendProtocol } from './protocol'
 
 export type SandboxProviderCreateOptions = {
   /**
    * Working space directory for the sandbox instance
    */
   workingDirectory?: string
-  /**
-   * Sandbox container environment identifier
-   */
-  environmentId?: string
+  // /**
+  //  * Canonical sandbox environment identifier that owns the container lifecycle.
+  //  */
+  // environmentId?: string
   /**
    * Tenant identifier
    */
   tenantId?: string
-  /**
-   * User identifier
-   */
-  userId?: string
-  /**
-   * Project identifier
-   */
-  projectId?: string
+  workFor: {
+    type: TSandboxWorkForType
+    id: string
+  }
+  // /**
+  //  * User identifier
+  //  */
+  // userId?: string
+  // /**
+  //  * Project identifier
+  //  */
+  // projectId?: string
 }
 
 export interface ISandboxProvider<T extends SandboxBackendProtocol = SandboxBackendProtocol> {
   type: string
-  
+
   meta: TSandboxProviderMeta
 
   /**
    * Create a new sandbox instance
-   * 
-   * @param options 
+   *
+   * @param options
    */
   create(options?: SandboxProviderCreateOptions): Promise<T>
   /**
